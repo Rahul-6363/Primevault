77e9a186bfc9a3a78f695fd2bc3088ca94bac049
Rahul-6363 <rrahulrahul220@gmail.com>
2025-07-02 19:37:37 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -30,7 +30,7 @@ import AdminResellers from './components/Admin/AdminResellers';
 import OrderConfirmationPage from './pages/OrderConfirmationPage';
 import ContactUs from './pages/ContactUs';
 import AboutUs from './pages/AboutUs';
-
+import { Analytics } from '@vercel/analytics/react';
 
 
 
@@ -88,7 +88,7 @@ const App = () => {
   
 
     </BrowserRouter>
-    
+    <Analytics />
     </Provider>
   );
 };
